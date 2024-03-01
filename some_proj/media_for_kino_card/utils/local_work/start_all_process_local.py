06c768bf48b0adff7a0eae2191eb1bfd09b6ef10
evgeny.izvekov <evgeny.izvekov@redcollar.ru>
2024-03-01 18:47:05 +0300

    Добавил celery

--- some_proj/media_for_kino_card/utils/local_work/start_all_process_local.py
+++ some_proj/media_for_kino_card/utils/local_work/start_all_process_local.py
@@ -11,24 +11,32 @@ def start_process_media_files_local(instance):
     orig_file_path = instance.orig_path_file
     qualities = Quality.objects.all()
     film = FilmModel.objects.get(pk=instance.object_id)
+
     # Определение соотношения разрешения оригинального фильма
-    correlation = get_video_stream(
+    correlation = get_video_stream.delay(
         orig_file_path,
     )
+    correlation_value = correlation.get(timeout=10)
     logging.info("Соотношение определено")
     for quality in qualities:
         # Кодирование видео
-        recording_file_path = recoding_files(
+        recording_file_path = recoding_files.delay(
             orig_file_path,
             film.name,
             quality.name,
-            correlation,
+            correlation_value,
         )
-        logging.info("Кодирование прошло успешно")
+        recording_file_value = recording_file_path.get(propagate=False)
+        success_msg_recording = f"Кодирование качества{quality.name} прошло успешно"
+        logging.info(success_msg_recording)
+
         # Создание и добавление в таблицу локальных ссылок видео-файлов
-        create_add_local_links(
-            instance,
-            quality,
-            recording_file_path,
+        instance_id = instance.object_id
+        quality_id = quality.pk
+        create_add_local_links.delay(
+            instance_id,
+            quality_id,
+            recording_file_value,
         )
-        logging.info("Локальные ссылки успешно созданы")
+        success_msg_create_link = f"Локальные ссылка для качества {quality.name} успешно создана"
+        logging.info(success_msg_create_link)
