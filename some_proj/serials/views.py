df818a89efa459b9776783ff5bf0e8c7c3f05a0d
evgeny.izvekov <evgeny.izvekov@redcollar.ru>
2024-03-13 13:13:07 +0300

    Добавил views для фильма и сериала

--- some_proj/serials/views.py
+++ some_proj/serials/views.py
@@ -1,1 +1,27 @@
-# Create your views here.
+from rest_framework import permissions
+from rest_framework import viewsets
+
+from some_proj.serials.models import SerialModel
+from some_proj.serials.serializers import AdminListAdminSerializer
+from some_proj.serials.serializers import AdminSerialSerializer
+from some_proj.serials.serializers import DetailedSerialGuestSerializer
+from some_proj.serials.serializers import DetailedSerialSerializer
+from some_proj.serials.serializers import ListSerialGuestSerializer
+from some_proj.serials.serializers import ListSerialSerializer
+
+
+class SerialsView(viewsets.ModelViewSet):
+    permission_classes = [permissions.AllowAny]
+    queryset = SerialModel.objects.all()
+
+    def get_serializer_class(self):
+        user = self.request.user
+        has_pk_param = bool(self.kwargs.get("pk"))
+
+        serializer_mapping = {
+            "authenticated": DetailedSerialSerializer if has_pk_param else ListSerialSerializer,
+            "anonymous": DetailedSerialGuestSerializer if has_pk_param else ListSerialGuestSerializer,
+            "staff": AdminSerialSerializer if has_pk_param else AdminListAdminSerializer,
+        }
+        role = "staff" if user.is_staff else "authenticated" if user.is_authenticated else "anonymous"
+        return serializer_mapping.get(role)
