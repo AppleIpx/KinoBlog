2a3d561190fe94ac8bb0d9ecc2b54127081fccc1
evgeny.izvekov <evgeny.izvekov@redcollar.ru>
2024-03-13 15:51:36 +0300

    Исправил пути сохранения фотографиий, добавил роутер и изменил лимит времени для celery

--- config/api_router.py
+++ config/api_router.py
@@ -2,11 +2,15 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter
 from rest_framework.routers import SimpleRouter
 
+from some_proj.films.views import FilmsView
+from some_proj.serials.views import SerialsView
 from some_proj.users.api.views import UserViewSet
 
 router = DefaultRouter() if settings.DEBUG else SimpleRouter()
 
 router.register("users", UserViewSet)
+router.register("films", FilmsView)
+router.register("serials", SerialsView)
 
 
 app_name = "api"
--- some_proj/media_for_kino_card/signals.py
+++ some_proj/media_for_kino_card/signals.py
@@ -3,14 +3,14 @@ from django.db.models.signals import post_save
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
-from some_proj.films.models import FilmContentModel
+from some_proj.films.models import FilmModel
 from some_proj.media_for_kino_card.models import MediaFile
 from some_proj.media_for_kino_card.utils.executing_processes_in_signal import start_signal_processes
 from some_proj.serials.models import SerialModel
 
 
 @receiver(post_save, sender=SerialModel)
-@receiver(post_save, sender=FilmContentModel)
+@receiver(post_save, sender=FilmModel)
 def create_media_file(sender, instance, created, **kwargs):
     content_type = ContentType.objects.get_for_model(sender)
     if created:
