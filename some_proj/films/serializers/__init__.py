342f53ae5f9da59d7c1ef5df4fe7664d3330938b
evgeny.izvekov <evgeny.izvekov@redcollar.ru>
2024-03-19 16:46:47 +0300

    Добавлены исключения для ruff

---
+++ some_proj/films/serializers/__init__.py
@@ -0,0 +1,5 @@
+from .actor_serializer import ActorSerializer  # noqa: F401
+from .country_serializer import CountrySerializer  # noqa: F401
+from .genry_serializer import GenreSerializer  # noqa: F401
+from .photo_film_serializer import PhotoFilmSerializer  # noqa: F401
+from .producer_serializer import ProducerSerializer  # noqa: F401
