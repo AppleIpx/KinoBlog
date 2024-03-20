f041a14ad42b698b7a17d5076bd6db9c33a7113f
evgeny.izvekov <evgeny.izvekov@redcollar.ru>
2024-03-21 00:34:25 +0300

    Изменил файл для старта всех процессов с S3

--- some_proj/media_for_kino_card/utils/starting_processes/start_all_process_s3.py
+++ some_proj/media_for_kino_card/utils/starting_processes/start_all_process_s3.py
@@ -7,7 +7,6 @@ from some_proj.media_for_kino_card.tasks import recoding_files
 from some_proj.media_for_kino_card.tasks import upload_to_s3
 from some_proj.media_for_kino_card.utils.shared_files import check_instance
 from some_proj.media_for_kino_card.utils.shared_files import create_add_links
-from some_proj.media_for_kino_card.utils.shared_files import create_add_links_for_amazon
 from some_proj.media_for_kino_card.utils.shared_files import create_links
 
 
@@ -17,10 +16,13 @@ def start_process_media_files_s3(instance):
     qualities = Quality.objects.all()
 
     # определение объекта: Фильм или Сериал
-    content_name = check_instance(instance)
+    content_name, file_name = check_instance(instance)
     content_name = content_name.replace(" ", "_")
+    file_name = file_name.replace(" ", "_")
+
     object_message = "Объект определен"
     logging.info(object_message)
+
     download_start_message = "Начало загрузки исходного файла с s3"
     logging.info(download_start_message)
 
@@ -30,8 +32,10 @@ def start_process_media_files_s3(instance):
         content_name,
     )
     orig_local_path_value = orig_local_path.get(timeout=400)
+
     download_message = "Оригинальный файл успешно скачан"
     logging.info(download_message)
+
     correlation_start_message = "Определяется соотношение исходного файла"
     logging.info(correlation_start_message)
 
@@ -40,8 +44,10 @@ def start_process_media_files_s3(instance):
         orig_local_path_value,
     )
     correlation_value = correlation.get(timeout=60)
+
     correlation_message = "Соотношение определено"
     logging.info(correlation_message)
+
     for quality in qualities:
         recording_start_message = f"Начало кодирования качества {quality.name}"
         logging.info(recording_start_message)
@@ -58,25 +64,25 @@ def start_process_media_files_s3(instance):
         logging.info(success_msg_recording)
 
         # создание ссылки для отображения в api
-        s3_url_for_model = create_links(content_name, quality)
+        # создание ссылки для загрузки файла внутри с3
+        s3_url_for_api, s3_url = create_links(quality, file_name)
 
         # Создание и добавление в таблицу ссылок видео-файлов на S3
         create_add_links(
             instance,
             quality,
-            s3_url_for_model,
+            s3_url_for_api,
         )
         success_msg_create_link = f"Ссылка для качества {quality.name} успешно создана"
         logging.info(success_msg_create_link)
 
-        # генерирование ссылки для s3
-        s3_url = create_add_links_for_amazon(content_name, quality)
-
         # загрузка перекодированных фильмов на S3
         uploading_message = f"Файл {instance.content_object.name} {quality.name} успешно загружается на s3"
         logging.info(uploading_message)
+
         answer = upload_to_s3.delay(recording_file_path_value, s3_url)
         answer_upload = answer.get(propagate=False)
+
         if answer_upload:
             answer_message = "Файл успешно загружен"
             logging.info(answer_message)
