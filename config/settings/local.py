d7c771a9393563c77d2aade6bd852396a2f15501
evgeny.izvekov <evgeny.izvekov@redcollar.ru>
2024-05-17 16:04:24 +0300

    test pipeline, добавлена настройка кэша

--- config/settings/local.py
+++ config/settings/local.py
@@ -21,10 +21,15 @@ ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-        "LOCATION": "",
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": env("CELERY_BROKER_URL"),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "IGNORE_EXCEPTIONS": True,
+        },
     },
 }
+
 
 # EMAIL
 # ------------------------------------------------------------------------------
