43bac93f10d2dfa6cdf875dea886dd4ef38fe12e
evgeny.izvekov <evgeny.izvekov@redcollar.ru>
2024-05-20 20:54:46 +0300

    test pipeline, исправлена ошибка с mypy

--- some_proj/films/models.py
+++ some_proj/films/models.py
@@ -240,8 +240,8 @@ class IsContentWatch(BaseUserRelation):
             return f"{self.user} смотрел {self.content_object.name} и остановился на {self.minutes}"
         if self.content_object and hasattr(self.content_object, "episode"):
             return (
-                f"{self.user} смотрел {self.content_object.name} и"  # type: ignore[union-attr]
-                f" остановился на {self.media.content_object.episode} серии на {self.minutes} минуте"
+                f"{self.user} смотрел {self.content_object.name} и"
+                f" остановился на {self.media.content_object.episode} серии на {self.minutes} минуте"  # type: ignore[union-attr]
             )
         return f"{self.content_object} не определён"
 
