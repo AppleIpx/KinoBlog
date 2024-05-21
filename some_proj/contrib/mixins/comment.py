91f76d7c8f3d45c0fccc497cd35e7cb65705de18
evgeny.izvekov <evgeny.izvekov@redcollar.ru>
2024-05-21 22:38:51 +0300

    test pipeline, добавлено content_type

--- some_proj/contrib/mixins/comment.py
+++ some_proj/contrib/mixins/comment.py
@@ -1,3 +1,4 @@
+from django.contrib.contenttypes.models import ContentType
 from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 
@@ -10,9 +11,10 @@ class CommentMixin(serializers.Serializer):
 
     @extend_schema_field(CommentSerializer)
     def get_comments(self, obj):
+        content_type = ContentType.objects.get_for_model(obj)
         comments_qs = CommentModel.objects.filter(
             object_id=obj.id,
-            content_type=obj.content_type,
+            content_type=content_type,
         ).prefetch_related("user")
 
         serializer = CommentSerializer(comments_qs, many=True)
